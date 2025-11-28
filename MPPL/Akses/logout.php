c40a681d0b338944da75bcbb51afc36ce757726f
Muhammad Fathurrohman Al-Hanif <fbhanif20@gmail.com>
2025-11-28 18:20:13 +0700

    update bagian USER

--- MPPL/Akses/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();       // Hapus semua variabel session
-session_destroy();     // Hapus semua session
-
-// Redirect ke halaman login
-header("Location: login.php");
-exit;
